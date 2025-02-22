d8b652d81ab58fc28cbc66b7917f9583ce9a374d
Ankush Notiyal <ankushnotiyal@Ankushs-MacBook-Air.local>
2025-02-22 18:32:35 +0100

    workflow

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket = "avi-hero-no-1"
     prefix = "terraform/state"
-    credentials = file("./account.json") # Credentials for backend storage
+    #credentials = file("./account.json") # Credentials for backend storage
   }
 }
